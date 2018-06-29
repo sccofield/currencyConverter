0da8c614abb388bb51ac9538ecc62ca3e6ea9dab
sccofield <michael_umanah@yahoo.com>
2018-06-29 18:40:21 +0100

    mend

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const registerServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then((registration) => {
+      navigator.serviceWorker.register('sw.js').then((registration) => {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, (err) => {
